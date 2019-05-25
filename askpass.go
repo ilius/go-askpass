e58232f0f16603db4769eaa8825f516b7dc01bb9
Saeed Rasooli <saeed.gnu@gmail.com>
2019-05-26 03:01:57 +0430

    add support for Linux (GUI) askpass programs, add comments

--- askpass.go
+++ askpass.go
@@ -22,6 +22,7 @@ package askpass
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"syscall"
 
@@ -29,16 +30,24 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-// Askpass prompts user for an interactive password.
-// If verify is true, confirm a second time.
-// Mistakes during confirmation cause the process to restart upto a
-// maximum of 2 times.
-func Askpass(prompt string, verify bool) (string, error) {
+func askpassOnce(prompt string, out *os.File) (string, error) {
+	askpassPath := getAskpassBinaryPath()
+
+	if askpassPath != "" {
+		stdout, stderr, exitCode := RunCommand3(askpassPath, prompt)
+		if exitCode != 0 {
+			if stderr != "" {
+				return "", fmt.Errorf("error from %v:\n%v", askpassPath, stderr)
+			}
+		}
+		if stdout == "" {
+			return "", fmt.Errorf("Entered empty password")
+		}
+		return stdout, nil
+	}
 	var fd int
-	var out *os.File
 	if terminal.IsTerminal(syscall.Stdin) {
 		fd = syscall.Stdin
-		out = os.Stdout
 	} else {
 		tty, err := os.Open("/dev/tty")
 		if err != nil {
@@ -47,19 +56,33 @@ func Askpass(prompt string, verify bool) (string, error) {
 		defer tty.Close()
 		fd = int(tty.Fd())
 	}
+	out.WriteString(prompt + ": ")
+	pw, err := terminal.ReadPassword(fd)
+	out.WriteString("\n")
+	if err != nil {
+		return "", err
+	}
+	return string(pw), nil
+}
+
+// Askpass prompts user for an interactive password.
+// If verify is true, confirm a second time.
+// Mistakes during confirmation cause the process to restart upto a
+// maximum of 2 times.
+// We will try to use one of GUI askpass programs (in Linux/Unix)
+// If none were found, will prompt in standard input
+// If standard input is redirected, will open a new tty for reading password
+func Askpass(prompt string, verify bool) (string, error) {
+	var out *os.File
 	if terminal.IsTerminal(syscall.Stdout) {
 		out = os.Stdout
 	} else if terminal.IsTerminal(syscall.Stderr) {
 		out = os.Stderr
 	} else {
 		return "", fmt.Errorf("neither stdout not stderr is a terminal")
-		// TODO: use askpass programs
 	}
-
 	for i := 0; i < 2; i++ {
-		out.WriteString(prompt + ": ")
-		pw1, err := terminal.ReadPassword(fd)
-		out.WriteString("\n")
+		pw1, err := askpassOnce(prompt, out)
 		if err != nil {
 			return "", err
 		}
@@ -67,9 +90,7 @@ func Askpass(prompt string, verify bool) (string, error) {
 			return string(pw1), nil
 		}
 
-		out.WriteString(prompt + " again: ")
-		pw2, err := terminal.ReadPassword(fd)
-		out.WriteString("\n")
+		pw2, err := askpassOnce(prompt+" again", out)
 		if err != nil {
 			return "", err
 		}
@@ -86,4 +107,31 @@ func Askpass(prompt string, verify bool) (string, error) {
 	return "", fmt.Errorf("Too many tries getting password")
 }
 
-// vim: ft=go:sw=8:ts=8:noexpandtab:tw=98:
+func getAskpassBinaryPath() string {
+	if os.Getenv("GO_ASKPASS") != "" {
+		askpassPath := os.Getenv("GO_ASKPASS")
+		_, err := os.Stat(askpassPath)
+		if err == nil {
+			return askpassPath
+		}
+		log.Println(err)
+	}
+	for _, askpassPath := range []string{
+		"/usr/lib/openssh/gnome-ssh-askpass",
+		"/usr/bin/ksshaskpass",
+		"/usr/bin/lxqt-openssh-askpass",
+		"/usr/libexec/openssh/lxqt-openssh-askpass",
+		"/usr/bin/ssh-askpass-fullscreen",
+		"/usr/lib/ssh/x11-ssh-askpass",
+	} {
+		_, err := os.Stat(askpassPath)
+		if err == nil {
+			return askpassPath
+		}
+		if os.IsNotExist(err) {
+			continue
+		}
+		log.Println(err)
+	}
+	return ""
+}
---
+++ cmd_utils.go
@@ -0,0 +1,46 @@
+package askpass
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+const defaultCommandFailedCode = 1
+
+func RunCommand3(name string, args ...string) (stdout string, stderr string, exitCode int) {
+	var outbuf, errbuf bytes.Buffer
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = &outbuf
+	cmd.Stderr = &errbuf
+
+	err := cmd.Run()
+	stdout = outbuf.String()
+	stderr = errbuf.String()
+
+	if err != nil {
+		// try to get the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			exitCode = ws.ExitStatus()
+		} else {
+			// This will happen (in OSX) if `name` is not available in $PATH,
+			// in this situation, exit code could not be get, and stderr will be
+			// empty string very likely, so we use the default fail code, and format err
+			// to string and set to stderr
+			exitCode = defaultCommandFailedCode
+			if stderr == "" {
+				stderr = err.Error()
+			}
+		}
+	} else {
+		// success, exitCode should be 0 if go is ok
+		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		exitCode = ws.ExitStatus()
+	}
+
+	stdout = strings.TrimSpace(stdout)
+	stderr = strings.TrimSpace(stderr)
+	return
+}
